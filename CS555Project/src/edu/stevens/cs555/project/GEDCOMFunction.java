faaf256688f2e7278db8e49a17815e44ccab5bf7
garrett9 <garrettshevach@gmail.com>
2015-10-03 18:21:08 -0400

    My previous commit didn't add the new files I made. This commit has them.

---
+++ CS555Project/src/edu/stevens/cs555/project/GEDCOMFunction.java
@@ -0,0 +1,48 @@
+package edu.stevens.cs555.project;
+
+import java.util.ArrayList;
+
+/**
+ * An abstract rule class to be extended from when implementing a custom rule.
+ */
+public abstract class GEDCOMFunction {
+	
+    	/**
+    	 * The ID of the rule.
+    	 */
+	protected String id;
+	
+	/**
+	 * An array list of validation exceptions that will hold any possible validation errors when running the GEDCOMFunction.
+	 */
+	private ArrayList<GEDCOMValidationException> validation_exceptions =  new ArrayList<GEDCOMValidationException>();
+	
+	/**
+	 * Test the rule against the provided list of families and individuals.
+	 * 
+	 * @param families The list of familes.
+	 * @param individuals The list of individuals.
+	 */
+	public abstract void run(Family[] families, Individual[] individuals);
+	
+	public void printAnomaly(String anomaly) {
+		System.out.println("Anomoly " + this.id + ": " + anomaly);
+	}
+	
+	/**
+	 * Adds a GEDCOMValidationException to the GEDCOMFunction's list of validations exceptions.
+	 * 
+	 * @param e The GEDCOMValidationException to add.
+	 */
+	public void addValidationException(GEDCOMValidationException e) {
+	    this.validation_exceptions.add(e);
+	}
+	
+	/**
+	 * Returns the GEDCOMValidationExceptions that occured when running the function. Returns null if the function hasn't been run with the {@link #run(Family[], Individual[])} method, or if there are no validation errors.
+	 * @return
+	 */
+	public ArrayList<GEDCOMValidationException> getValidationErrors() {
+	    return this.validation_exceptions;
+	}
+}
---
+++ CS555Project/src/edu/stevens/cs555/project/GEDCOMValidationException.java
@@ -0,0 +1,42 @@
+package edu.stevens.cs555.project;
+
+/**
+ * A Custom Exception to be thrown when a validation error occurs in a provided GEDCOM file.
+ */
+public class GEDCOMValidationException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The line number of the record the validation error has occured with.
+     */
+    private int line_number;
+    
+    /**
+     * Initialize a new instance of a GEDCOMValidationException.
+     * 
+     * @param message The message of the exception.
+     * @param line_number The line number in the GEDCOM file of the record the validation error has occured with.
+     */
+    public GEDCOMValidationException(String message, int line_number) {
+	super(message);
+	this.line_number = line_number;
+    }
+    
+    /**
+     * Return the line number of the record the validation error has occured on.
+     * 
+     * @return The line number of the record the validation error has occured on.
+     */
+    public int getLineNumber() {
+	return this.line_number;
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Throwable#toString()
+     */
+    public String toString() {
+	return "Error with the record at line " + this.getLineNumber() + ": " + this.getMessage();
+    }
+}
